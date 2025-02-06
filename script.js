5b8228c342fd96de238c57603cd3ae782cf59879
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-06 12:33:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ function firstWord(s) {
         return s;
     }
     let words = s.split(" "); 
-    if(words[0] == ''){
-        return words[1]
-    }
+
     return words[0]
   }
   const s = prompt("Enter String:");
