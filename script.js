3d0445497acda74c8b5d737b9584789b05fda84a
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-06 12:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	   return "";
+  }
+
+  const firstSpaceIndex = str.indexOf(" ");
+
+  if (firstSpaceIndex === -1) {
+    return str;
+  } else {
+    return str.substring(0, firstSpaceIndex);
+  }
 }
 
 // Do not change the code below
